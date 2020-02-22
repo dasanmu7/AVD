f69f006f245de24d3b53db790c7c48a118971dab
David Sanchis <dasanmu7@gmail.com>
2020-02-22 11:35:58 +0100

    UI Task

---
+++ AVD/Assets/David Sanchis/Exitandplay.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Exitandplay : MonoBehaviour
+{
+    public void doExitGame()
+    {
+        Application.Quit();
+    }
+
+    public void playButton(string scene)
+    {
+        SceneManager.LoadScene(scene);
+    }
+}
